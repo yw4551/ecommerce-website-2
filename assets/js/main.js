51ebf00a7d7c8b3ddfbcd46cafb22276bb36e2ea
yw4551 <yitziw123@gmail.com>
2024-10-29 17:30:12 +0200

    com

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,29 @@
 /*=============== IMAGE GALLERY ===============*/
 
 /*=============== SWIPER CATEGORIES ===============*/
+var swiperCategories = new Swiper(".categories-container", {
+    spaceBetween: 24,
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 24,
+        },
+      },
+});
 
 /*=============== SWIPER PRODUCTS ===============*/
 
